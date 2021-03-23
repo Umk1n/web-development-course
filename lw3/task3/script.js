549baa66c6a4e5ae13d4f7a8330dc41b74d36904
Umk1n <ishimaev@mail.ru>
2021-03-23 07:14:38 +0300

    Fixed task 1 and task 3

--- lw3/task3/script.js
+++ lw3/task3/script.js
@@ -1,43 +1,51 @@
 var Player = {
-    trackList: [
-      'song1.mp3',
-      'song2.mp3',
-      'song3.mp3',
-    ],
-    currentTrack: 'song1.mp3',
-    currentIndex: 0,
-    status: 'pause',
-    display: function() {
-      return 'Track: ' + this.currentTrack + ' Status: ' + this.status;
-    },
-    play: function() {
-      this.status = 'play';
-      console.log('Play');
-    },
-    pause: function() {
-      this.status = 'pause';
-      console.log('Pause');
-    },
-    next: function() {
-      if(this.currentIndex +1 >= this.trackList.length) {
-        this.currentIndex = 0;
-      }
-      else{
-        this.currentIndex += 1;
-      }
-      this.currentTrack = this.trackList[this.currentIndex];
-    },
-    prev: function() {
-      if(this.currentIndex  <= 0) {
-        this.currentIndex = this.trackList.length - 1;
-      }
-      else{
-        this.currentIndex -= 1;
-      }
-      this.currentTrack = this.trackList[this.currentIndex];
-    },
-    showTracks: function() {
-      console.log(this.trackList);
-      console.log(this.currentTrack + ' - Играет сейчас');
+  trackList: [
+    'song1.mp3',
+    'song2.mp3',
+    'song3.mp3',
+  ],
+  currentTrack: 'song1.mp3',
+  currentIndex: 0,
+  status: 'pause',
+  display: function() {
+    return 'Track: ' + this.currentTrack + ' Status: ' + this.status;
+  },
+  play: function() {
+    this.status = 'play';
+    console.log('Play');
+  },
+  pause: function() {
+    this.status = 'pause';
+    console.log('Pause');
+  },
+  next: function() {
+    if(this.currentIndex +1 >= this.trackList.length) {
+      this.currentIndex = 0;
     }
-  };
+    else {
+      this.currentIndex += 1;
+    }
+    this.currentTrack = this.trackList[this.currentIndex];
+    this.status = 'play';
+  },
+  prev: function() {
+    if (this.currentIndex  <= 0) {
+      this.currentIndex = this.trackList.length - 1;
+    }
+    else {
+      this.currentIndex -= 1;
+    }
+    this.currentTrack = this.trackList[this.currentIndex];
+    this.status = 'play';
+  },
+  showTracks: function() {
+    Musicthis = this.trackList.map(song => {
+      if (song === this.currentTrack) {
+        console.log(song + ' - Играет сейчас')
+      }
+      else {
+        console.log(song)
+      }
+    })
+  }
+};
